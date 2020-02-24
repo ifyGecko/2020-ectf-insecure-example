344333406f1c980bb68b62955e91c9bdb33d9d05
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2020-02-24 17:40:43 -0600

    fixed memset call for char*

--- mb/drm_audio_fw/src/main.c
+++ mb/drm_audio_fw/src/main.c
@@ -515,8 +515,8 @@ int main() {
     memset(c->drm_state, 0, sizeof(char));
     memset(c->login_status, 0, sizeof(char));
     memset(c->padding, 0, sizeof(char));
-    memset(c->username, 0, sizeof(char));
-    memset(c->pin, 0, sizeof(char));
+    memset(c->username, 0, sizeof(char*));
+    memset(c->pin, 0, sizeof(char*));
     memset(c->query, 0, sizeof(query));
     
     mb_printf("Audio DRM Module has Booted\n\r");
